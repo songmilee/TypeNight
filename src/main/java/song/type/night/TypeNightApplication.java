194608f1e0624faf894051c20a09486bbe718383
songmilee <lsm941010@gmail.com>
2018-08-29 20:42:33 +0900

    spring boot

---
+++ src/main/java/song/type/night/TypeNightApplication.java
@@ -0,0 +1,15 @@
+package song.type.night;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class TypeNightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeNightApplication.class, args);
+	}
+}
